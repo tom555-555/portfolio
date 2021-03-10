ce86fb8223b34950a57779c08323373dd4a67cf1
Tomoki <tomok.development@gmail.com>
2021-03-10 21:15:50 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+new Vue({
+  el: '.sns_links',
+  data: {
+    instagram_url: 'https://www.instagram.com/hi_im_tomonky/?hl=ja',
+    facebook_url: 'https://www.facebook.com/tomo.ka.161',
+    github_url:'https://github.com/tom555-555'
+  }
+})
